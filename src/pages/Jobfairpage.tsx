2cb2ee61bee32580569aa9b8fc17bb55a662bd8a
Nikhil <nikhilade4040@gmail.com>
2025-09-30 10:06:31 +0530

    Updated images and added video in the Job Fair section

--- src/pages/Jobfairpage.tsx
+++ src/pages/Jobfairpage.tsx
@@ -12,17 +12,14 @@ import kdk4 from "../assests/job-fair-images/KDK-college/4.png";
 import EastImage1 from "../assests/job-fair-images/Job-Fair-East-Nagpur/1.png";
 import EastImage2 from "../assests/job-fair-images/Job-Fair-East-Nagpur/2.png";
 import EastImage3 from "../assests/job-fair-images/Job-Fair-East-Nagpur/3.png";
-import EastImage4 from "../assests/job-fair-images/Job-Fair-East-Nagpur/4.png";
-import EastImage5 from "../assests/job-fair-images/Job-Fair-East-Nagpur/5.png";
-import EastImage6 from "../assests/job-fair-images/Job-Fair-East-Nagpur/6.png";
-import ImageBuilding1 from "../assests/job-fair-images/Job-fair-image-building/1.png";
-import ImageBuilding2 from "../assests/job-fair-images/Job-fair-image-building/2.png";
-import ImageBuilding3 from "../assests/job-fair-images/Job-fair-image-building/3.png";
+import EastImage7 from "../assests/job-fair-images/Job-Fair-East-Nagpur/7.jpg";
+import EastImage11 from "../assests/job-fair-images/Job-Fair-East-Nagpur/11.png";
 import ImageBuilding4 from "../assests/job-fair-images/Job-fair-image-building/4.png";
 import ImageBuilding5 from "../assests/job-fair-images/Job-fair-image-building/5.png";
 import ImageBuilding6 from "../assests/job-fair-images/Job-fair-image-building/6.png";
 import ImageBuilding7 from "../assests/job-fair-images/Job-fair-image-building/7.png";
 import ImageBuilding8 from "../assests/job-fair-images/Job-fair-image-building/8.png";
+import ImageBuilding9 from "../assests/job-fair-images/Job-fair-image-building/9.png";
 import ImageBuilding10 from "../assests/job-fair-images/Job-fair-image-building/10.png";
 import AmravaiFair1 from "../assests/job-fair-images/Job-fair-Amravati/1.png";
 import AmravaiFair2 from "../assests/job-fair-images/Job-fair-Amravati/2.png";
@@ -30,32 +27,15 @@ import AmravaiFair3 from "../assests/job-fair-images/Job-fair-Amravati/3.png";
 import AmravaiFair4 from "../assests/job-fair-images/Job-fair-Amravati/4.png";
 import AmravaiFair5 from "../assests/job-fair-images/Job-fair-Amravati/5.png";
 import AmravaiFair6 from "../assests/job-fair-images/Job-fair-Amravati/6.png";
-import AmravaiFair7 from "../assests/job-fair-images/Job-fair-Amravati/7.png";
-import AmravaiFair8 from "../assests/job-fair-images/Job-fair-Amravati/8.png";
-import AmravaiFair9 from "../assests/job-fair-images/Job-fair-Amravati/9.png";
-import AmravaiFair10 from "../assests/job-fair-images/Job-fair-Amravati/10.png";
+import AmravaiFair10 from "../assests/job-fair-images/Job-fair-Amravati/7.png";
 import AmravaiFair11 from "../assests/job-fair-images/Job-fair-Amravati/11.png";
 import AmravaiFair12 from "../assests/job-fair-images/Job-fair-Amravati/12.png";
-import AmravaiFair13 from "../assests/job-fair-images/Job-fair-Amravati/13.png";
 import AmravaiFair14 from "../assests/job-fair-images/Job-fair-Amravati/14.png";
 import AmravaiFair15 from "../assests/job-fair-images/Job-fair-Amravati/15.png";
 import AmravaiFair16 from "../assests/job-fair-images/Job-fair-Amravati/16.png";
 import Rural1 from "../assests/job-fair-images/Campus-drive-rural-area/1.png";
 import Rural2 from "../assests/job-fair-images/Campus-drive-rural-area/2.png";
 import Rural3 from "../assests/job-fair-images/Campus-drive-rural-area/3.png";
-import Rural4 from "../assests/job-fair-images/Campus-drive-rural-area/4.png";
-import Rural5 from "../assests/job-fair-images/Campus-drive-rural-area/5.png";
-import Rural6 from "../assests/job-fair-images/Campus-drive-rural-area/6.png";
-import Rural7 from "../assests/job-fair-images/Campus-drive-rural-area/7.png";
-import Rural9 from "../assests/job-fair-images/Campus-drive-rural-area/9.png";
-import Rural10 from "../assests/job-fair-images/Campus-drive-rural-area/10.png";
-import Rural11 from "../assests/job-fair-images/Campus-drive-rural-area/11.png";
-import Rural12 from "../assests/job-fair-images/Campus-drive-rural-area/12.png";
-import Rural13 from "../assests/job-fair-images/Campus-drive-rural-area/13.png";
-import Rural14 from "../assests/job-fair-images/Campus-drive-rural-area/14.png";
-import Rural15 from "../assests/job-fair-images/Campus-drive-rural-area/15.png";
-import Rural16 from "../assests/job-fair-images/Campus-drive-rural-area/16.png";
-import Rural18 from "../assests/job-fair-images/Campus-drive-rural-area/18.png";
 import Fair17Img1 from "../assests/job-fair-images/Job-Fair-2017/1.png";
 import Fair17Img2 from "../assests/job-fair-images/Job-Fair-2017/2.png";
 import Fair17Img3 from "../assests/job-fair-images/Job-Fair-2017/3.png";
@@ -66,16 +46,58 @@ import Campus15Img from "../assests/job-fair-images/Job-Fair-2015/1.png";
 import Campus20Img1 from "../assests/job-fair-images/Campus-Drive-2020/1.png";
 import Campus20Img2 from "../assests/job-fair-images/Campus-Drive-2020/2.png";
 import Campus20Img3 from "../assests/job-fair-images/Campus-Drive-2020/3.png";
-import Wani1 from "../assests/job-fair-images/Job-Fair-Wani/1.png";
-import Wani2 from "../assests/job-fair-images/Job-Fair-Wani/2.png";
-import Wani3 from "../assests/job-fair-images/Job-Fair-Wani/3.png";
-import Wani4 from "../assests/job-fair-images/Job-Fair-Wani/4.png";
+import ImageBuild2 from "../assests/job-fair-images/slide 1/2.png";
+import ImageBuild3 from "../assests/job-fair-images/slide 1/3.png";
+import ImageBuild4 from "../assests/job-fair-images/slide 1/4.png";
+import ImageBuild5 from "../assests/job-fair-images/slide 1/5.png";
+import ImageBuild6 from "../assests/job-fair-images/slide 1/6.png";
+import MnsImg1 from "../assests/job-fair-images/MNS-Yawatmal/1.jpg";
+import MnsImg2 from "../assests/job-fair-images/MNS-Yawatmal/2.jpg";
+import MnsImg3 from "../assests/job-fair-images/MNS-Yawatmal/3.png";
+import MnsImg4 from "../assests/job-fair-images/MNS-Yawatmal/4.jpg";
+import MnsImg5 from "../assests/job-fair-images/MNS-Yawatmal/5.jpg";
+import MnsImg6 from "../assests/job-fair-images/MNS-Yawatmal/6.png";
 import Umred1 from "../assests/job-fair-images/Job-Fair-Umred/1.png";
 import Umred2 from "../assests/job-fair-images/Job-Fair-Umred/2.png";
-import Umred3 from "../assests/job-fair-images/Job-Fair-Umred/3.png";
-import Umred4 from "../assests/job-fair-images/Job-Fair-Umred/4.png";
+import Umred3 from "../assests/job-fair-images/Job-Fair-Umred/4.png";
+import Umred4 from "../assests/job-fair-images/Job-Fair-Umred/5.png";
+import Chandrapur1 from "../assests/job-fair-images/Mega Job Fair Chandrapur/img1.jpg";
+import Chandrapur2 from "../assests/job-fair-images/Mega Job Fair Chandrapur/img2.jpg";
+import Chandrapur3 from "../assests/job-fair-images/Mega Job Fair Chandrapur/4.png";
+import Chandrapur4 from "../assests/job-fair-images/Mega Job Fair Chandrapur/12.png";
+import Chandrapur5 from "../assests/job-fair-images/Mega Job Fair Chandrapur/13.png";
+import Chandrapur6 from "../assests/job-fair-images/Mega Job Fair Chandrapur/14.png";
+import Chandrapur7 from "../assests/job-fair-images/Mega Job Fair Chandrapur/15.png";
+import Chandrapur8 from "../assests/job-fair-images/Mega Job Fair Chandrapur/7.png";
+import jobFairGirish1 from "../assests/job-fair-images/Job-Fair-Radhikatai/16.png";
+import jobFairGirish2 from "../assests/job-fair-images/Job-Fair-Radhikatai/17.png";
+import jobFairGirish3 from "../assests/job-fair-images/Job-Fair-Radhikatai/18.png";
+import jobFairVideo from "../assests/job-fair-images/Job-fair-image-building/video1.mp4";
+import mnsVideo from "../assests/job-fair-images/MNS-Yawatmal/video1.mp4";
 
 function Jobfairpage() {
+  const ImageBuilding = [
+    {
+      title: "image2",
+      image: ImageBuild2,
+    },
+    {
+      title: "image3",
+      image: ImageBuild3,
+    },
+    {
+      title: "image4",
+      image: ImageBuild4,
+    },
+    {
+      title: "image5",
+      image: ImageBuild5,
+    },
+    {
+      title: "image6",
+      image: ImageBuild6,
+    },
+  ];
   const campusDriveRural = [
     {
       title: "image1",
@@ -88,58 +110,6 @@ function Jobfairpage() {
     {
       title: "image3",
       image: Rural3,
-    },
-    {
-      title: "image4",
-      image: Rural4,
-    },
-    {
-      title: "image5",
-      image: Rural5,
-    },
-    {
-      title: "image6",
-      image: Rural6,
-    },
-    {
-      title: "image7",
-      image: Rural7,
-    },
-    {
-      title: "image10",
-      image: Rural9,
-    },
-    {
-      title: "image10",
-      image: Rural10,
-    },
-    {
-      title: "image11",
-      image: Rural11,
-    },
-    {
-      title: "image12",
-      image: Rural12,
-    },
-    {
-      title: "image13",
-      image: Rural13,
-    },
-    {
-      title: "image14",
-      image: Rural14,
-    },
-    {
-      title: "image15",
-      image: Rural15,
-    },
-    {
-      title: "image16",
-      image: Rural16,
-    },
-    {
-      title: "image18",
-      image: Rural18,
     },
   ];
   const jobFairAmravati = [
@@ -168,32 +138,16 @@ function Jobfairpage() {
       image: AmravaiFair6,
     },
     {
-      title: "image7",
-      image: AmravaiFair7,
-    },
-    {
-      title: "image8",
-      image: AmravaiFair8,
-    },
-    {
-      title: "image10",
-      image: AmravaiFair9,
-    },
-    {
       title: "image10",
       image: AmravaiFair10,
     },
     {
-      title: "image10",
+      title: "image11",
       image: AmravaiFair11,
     },
     {
-      title: "image10",
+      title: "image12",
       image: AmravaiFair12,
-    },
-    {
-      title: "image10",
-      image: AmravaiFair13,
     },
     {
       title: "image10",
@@ -210,16 +164,8 @@ function Jobfairpage() {
   ];
   const jobFairImgBuilding = [
     {
-      title: "image1",
-      image: ImageBuilding1,
-    },
-    {
-      title: "image2",
-      image: ImageBuilding2,
-    },
-    {
-      title: "image3",
-      image: ImageBuilding3,
+      title:"video1",
+      video: jobFairVideo
     },
     {
       title: "image4",
@@ -242,6 +188,10 @@ function Jobfairpage() {
       image: ImageBuilding8,
     },
     {
+      title: "image9",
+      image: ImageBuilding9,
+    },
+    {
       title: "image10",
       image: ImageBuilding10,
     },
@@ -260,16 +210,12 @@ function Jobfairpage() {
       image: EastImage3,
     },
     {
-      title: "image4",
-      image: EastImage4,
-    },
-    {
-      title: "image5",
-      image: EastImage5,
+      title: "image7",
+      image: EastImage7,
     },
     {
       title: "image6",
-      image: EastImage6,
+      image: EastImage11,
     },
   ];
   const campusDriveColleges = [
@@ -350,69 +296,156 @@ function Jobfairpage() {
       image: Campus20Img3,
     },
   ];
-  const jobFairWaniUmred = [
+  const jobFairUmred = [
     {
       title: "image1",
-      image: Wani1,
-    },
-    {
-      title: "image2",
-      image: Wani2,
-    },
-    {
-      title: "image3",
-      image: Wani3,
-    },
-    {
-      title: "image4",
-      image: Wani4,
-    },
-    {
-      title: "image5",
       image: Umred1,
     },
     {
-      title: "image6",
+      title: "image2",
       image: Umred2,
     },
     {
-      title: "image7",
+      title: "image3",
       image: Umred3,
     },
     {
-      title: "image8",
+      title: "image4",
       image: Umred4,
+    },
+  ];
+  const jobFairChandrapur = [
+    {
+      title: "image1",
+      image: Chandrapur3,
+    },
+    {
+      title: "image2",
+      image: Chandrapur8,
+    },
+    {
+      title: "image3",
+      image: Chandrapur6,
+    },
+    {
+      title: "image4",
+      image: Chandrapur7,
+    },
+    {
+      title: "image5",
+      image: Chandrapur4,
+    },
+    {
+      title: "image6",
+      image: Chandrapur5,
+    },
+    {
+      title: "image7",
+      image: Chandrapur1,
+    },
+    {
+      title: "image7",
+      image: Chandrapur2,
+    },
+  ];
+  const MnsJobFair = [
+    {
+      title: "video1",
+      video: mnsVideo,
+    },
+    {
+      title: "image1",
+      image: MnsImg1,
+    },
+    {
+      title: "image2",
+      image: MnsImg2,
+    },
+    {
+      title: "image3",
+      image: MnsImg3,
+    },
+    {
+      title: "image4",
+      image: MnsImg4,
+    },
+    {
+      title: "image4",
+      image: MnsImg5,
+    },
+    {
+      title: "image5",
+      image: MnsImg6,
+    },
+  ];
+  const JobFairRadhikaTai = [
+    {
+      title: "image2",
+      image: jobFairGirish2,
+    },
+    {
+      title: "image3",
+      image: jobFairGirish3,
+    },
+    {
+      title: "image1",
+      image: jobFairGirish1,
     },
   ];
   return (
     <div className="bg-gray-50">
       <JobFairBanner />
+      {/* Done */}
       <JobFairCarousel
-        title="Training Program and Job Fair"
+        title="Rojgar Medava - Shrikant Jichkar Foundation"
         jobFairs={jobFairImgBuilding}
       />
+      {/* Done */}
+      <JobFairCarousel
+        title="Training program with Image Building and Job Fair"
+        jobFairs={ImageBuilding}
+      />
+      {/* Done */}
       <JobFairCarousel
         title="East Nagpur Job Fair"
         jobFairs={jobFairEastNagpur}
       />
       <JobFairCarousel
-        title="Job Fair Amravati | Over 9000 Students Participated"
+        title="Job Fair Organized by Girish Pandav"
+        jobFairs={JobFairRadhikaTai}
+      />
+      {/* Done */}
+      <JobFairCarousel
+        title="MNS Organized Job Fair Wani,Yavatmal"
+        jobFairs={MnsJobFair}
+      />
+      {/* Done */}
+      <JobFairCarousel
+        title="Yashomati Tai Thakur at Job Fair Amravati 2023"
         jobFairs={jobFairAmravati}
       />
       <JobFairCarousel
-        title="Job Fair Wani and Umred"
-        jobFairs={jobFairWaniUmred}
+        title="Job Fair Chandrapur - Vijaybhau Wadettiwar"
+        jobFairs={jobFairChandrapur}
       />
+      <JobFairCarousel
+        title="Job Fair Umred - Rajubhau Parwe"
+        jobFairs={jobFairUmred}
+      />
+      {/* Done */}
       <JobFairCarousel title="Job Fair 2017-2018" jobFairs={jobFair1718} />
 
+      {/* Done */}
       <JobFairCarousel
         title="Campus Drive at various Colleges"
         jobFairs={campusDriveColleges}
       />
+      {/* Done */}
       <JobFairCarousel
         title="Campus Drive For Rural Areas Students"
         jobFairs={campusDriveRural}
       />
+      {/* Done */}
       <JobFairCarousel
         title="Campus Drive 2015 and 2020"
         jobFairs={campusDrive1520}
